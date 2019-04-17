bd187b2ac08c6107bbc02c5ec5cade8fbcbd7ef6
ViktoriaB2018 <bvo2008@mail.ru>
2019-04-17 13:33:51 +0300

    Create rooms_controller & change application_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  before_create :generate_nickname
-
   has_many :messages
+
+  before_create :generate_nickname
 
   def generate_nickname
     self.nickname = Faker::Name.first_name.downcase
